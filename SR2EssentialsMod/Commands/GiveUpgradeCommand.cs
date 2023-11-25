7bbba6569ae4617ae8bb3989d5f85cc447834384
MadMincraftr <thean.mcgarity@gmail.com>
2023-11-24 23:10:35 -0600

    Optimize usings + new command
    Adds the Give Upgrade command

---
+++ SR2EssentialsMod/Commands/GiveUpgradeCommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace SR2E.Commands
+{
+    public class GiveUpgradeCommand : SR2CCommand
+    {
+        public override string ID => "giveupgrade";
+
+        public override string Usage => "giveupgrade <id>";
+
+        public override string Description => "Gives an upgrade";
+
+        public override bool Execute(string[] args)
+        {
+            if (args == null || args.Length > 1)
+            {
+                MelonLogger.Error("Incorrect number of arguments!");
+                return false;
+            }
+
+            UpgradeDefinition id = Resources.FindObjectsOfTypeAll<UpgradeDefinition>().FirstOrDefault(x => x.ValidatableName.Equals(args[0]));
+            if (id == null)
+                throw new ArgumentException("This ID is incorrect");
+
+            SceneContext.Instance.PlayerState._model.upgradeModel.IncrementUpgradeLevel(id);
+
+            return true;
+        }
+
+        public override List<string> GetAutoComplete(int argIndex, string[] args)
+        {
+            if (argIndex == 0)
+            {
+                var identifiableTypeGroup =
+                    Resources.FindObjectsOfTypeAll<UpgradeDefinition>().Select(x => x.ValidatableName);
+                return identifiableTypeGroup.ToList();
+            }
+
+            return null;
+        }
+    }
+}
