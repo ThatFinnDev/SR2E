e977a72f389e748874975bbb2356a307d3a53f64
MadMincraftr <thean.mcgarity@gmail.com>
2023-11-26 17:15:48 -0600

    remove unneeded catch arguments

--- SR2EssentialsMod/Commands/GiveCommand.cs
+++ SR2EssentialsMod/Commands/GiveCommand.cs
@@ -32,7 +32,7 @@
                             }
                         }
                     }
-                    catch (Exception ignored) { }
+                    catch  { }
 
                 }
 
