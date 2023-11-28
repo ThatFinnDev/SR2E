9155426d24d853d0782c8ac534bc0cf7f0adc05a
ThatFinn <finn@thatfinn.dev>
2023-11-28 20:35:15 +0100

    FIx Infinite Slime Give Glitch

---
+++ SR2EssentialsMod/HarmonyPatches.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+
+namespace SR2E;
+
+[HarmonyPatch(typeof(WeaponVacuum), nameof(WeaponVacuum.Expel), typeof(GameObject), typeof(bool), typeof(float), typeof(SlimeAppearance.AppearanceSaveSet))]
+internal class WeaponVacuumExpelPatch
+{
+    public static void Prefix(WeaponVacuum __instance, ref bool ignoreEmotions)
+    {
+        if (__instance._player.Ammo.GetSelectedEmotions() == null)
+        {
+            ignoreEmotions = true;
+        }
+    }
+}
