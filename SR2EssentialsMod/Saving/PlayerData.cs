9888148e18bd1d1e67f6baf9952324f28a898a94
MadMincraftr <thean.mcgarity@gmail.com>
2023-12-03 12:10:49 -0600

    moving stuff

---
+++ SR2EssentialsMod/Saving/PlayerData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SR2E.Saving
+{
+
+    [Serializable]
+    public struct SR2EPlayerData
+    {
+        public bool noclipState;
+    }
+}
---
+++ SR2EssentialsMod/Saving/SlimeDataSaver.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static SR2E.Saving.SR2ESavableData;
+
+namespace SR2E.Saving
+{
+    public class SR2ESlimeDataSaver : MonoBehaviour
+    {
+        public void SaveData()
+        {
+            var model = GetComponent<IdentifiableActor>();
+
+            var data = new SR2ESlimeData()
+            {
+                scaleX = transform.localScale.x,
+                scaleY = transform.localScale.y,
+                scaleZ = transform.localScale.z,
+            };
+
+            if (Instance.slimeSavedData.ContainsKey(model.GetActorId()))
+            {
+                Instance.slimeSavedData[model.GetActorId()] = data;
+            }
+            else
+            {
+                Instance.slimeSavedData.Add(model.GetActorId(), data);
+            }
+        }
+
+        public static void LoadData()
+        {
+            var model = SceneContext.Instance.GameModel.identifiables;
+
+            foreach (var slime in Instance.slimeSavedData)
+            {
+                if (model.ContainsKey(slime.Key))
+                {
+                    var slimeTransform = model[slime.Key].GetGameObject().transform;
+                    var slimeData = slime.Value;
+
+                    slimeTransform.localScale = new Vector3(slimeData.scaleX, slimeData.scaleY, slimeData.scaleZ);
+                }
+            }
+
+        }
+    }
+}
