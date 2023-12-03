9afba57de602ca4ceb2007c00efbf0773e4e90cf
ThatFinn <finn@thatfinn.dev>
2023-12-03 16:59:07 +0100

    SilentExecute for some commands + Bug Fixes + Small Refactoring

--- SR2EssentialsMod/Commands/ClearInventoryCommand.cs
+++ SR2EssentialsMod/Commands/ClearInventoryCommand.cs
@@ -1,34 +1,35 @@
-﻿namespace SR2E.Commands
+﻿namespace SR2E.Commands;
+
+public class ClearInventoryCommand : SR2CCommand
 {
-    public class ClearInventoryCommand : SR2CCommand
+    public override string ID => "clearinv";
+    public override string Usage => "clearinv";
+    public override string Description => "Clears your inventory";
+
+    public override List<string> GetAutoComplete(int argIndex, string[] args)
     {
-        public override string ID => "clearinv";
-        public override string Usage => "clearinv";
-        public override string Description => "Clears your inventory";
-        public override List<string> GetAutoComplete(int argIndex, string[] args)
-        {
-            return null;
-        }
-        public override bool Execute(string[] args)
-        {
-            if (args != null)
-            {
-                SR2Console.SendError($"The '<color=white>{ID}</color>' command takes no arguments");
-                return false;
-            }
+        return null;
+    }
 
-            
-            if (SceneContext.Instance == null) { SR2Console.SendError("Load a save first!"); return false; }
-            if (SceneContext.Instance.PlayerState == null) { SR2Console.SendError("Load a save first!"); return false; }
+    public override bool Execute(string[] args)
+    {
+        if (args != null)
+        { SR2Console.SendError($"The '<color=white>{ID}</color>' command takes no arguments"); return false; }
 
+        if (!SR2EUtils.inGame) { SR2Console.SendError("Load a save first!"); return false; }
 
-            foreach (Ammo.Slot slot in SceneContext.Instance.PlayerState.Ammo.Slots)
-                slot.Clear();
-            
-            
-            SR2Console.SendMessage("Successfully cleared your inventory");
+        foreach (Ammo.Slot slot in SceneContext.Instance.PlayerState.Ammo.Slots)
+            slot.Clear();
+        
+        SR2Console.SendMessage("Successfully cleared your inventory");
+        return true;
+    }
 
-            return true;
-        }
+    public override bool SilentExecute(string[] args)
+    {
+        if (!SR2EUtils.inGame) return true;
+        foreach (Ammo.Slot slot in SceneContext.Instance.PlayerState.Ammo.Slots)
+            slot.Clear();
+        return true;
     }
 }
---
+++ SR2EssentialsMod/Commands/ConsoleVisibilityCommands.cs
@@ -0,0 +1,77 @@
+﻿namespace SR2E.Commands;
+
+public class ConsoleVisibilityCommands
+{
+    internal static void RegisterAllConsoleVisibilityCommands()
+    {
+        SR2Console.RegisterCommand(new OpenConsoleCommand());
+        SR2Console.RegisterCommand(new CloseConsoleCommand());
+        SR2Console.RegisterCommand(new ToggleConsoleCommand());
+    }
+    internal class OpenConsoleCommand : SR2CCommand
+    {
+        public override string ID => "openconsole";
+        public override string Usage => "openconsole";
+        public override string Description => "Opens the console";
+        public override bool executeWhenConsoleIsOpen => true;
+        public override List<string> GetAutoComplete(int argIndex, string[] args)
+        {
+            return null;
+        }
+        public override bool Execute(string[] args)
+        {
+            if (args != null)
+            { SR2Console.SendError($"The '<color=white>{ID}</color>' command takes no arguments"); return false; }
+            return SilentExecute(args);
+        }
+        public override bool SilentExecute(string[] args)
+        {
+            if(!SR2Console.isOpen)
+                SR2Console.Open();
+            return true;
+        }
+    }
+    internal class CloseConsoleCommand : SR2CCommand
+    {
+        public override string ID => "closeconsole";
+        public override string Usage => "closeconsole";
+        public override string Description => "Closes the console";
+        public override List<string> GetAutoComplete(int argIndex, string[] args)
+        {
+            return null;
+        }
+        public override bool Execute(string[] args)
+        {
+            if (args != null)
+            { SR2Console.SendError($"The '<color=white>{ID}</color>' command takes no arguments"); return false; }
+            return SilentExecute(args);
+        }
+        public override bool SilentExecute(string[] args)
+        {
+            if(SR2Console.isOpen)
+                SR2Console.Close();
+            return true;
+        }
+    }
+    internal class ToggleConsoleCommand : SR2CCommand
+    {
+        public override string ID => "toggleconsole";
+        public override string Usage => "toggleconsole";
+        public override string Description => "Toggles the console";
+        public override List<string> GetAutoComplete(int argIndex, string[] args)
+        {
+            return null;
+        }
+        public override bool Execute(string[] args)
+        {
+            if (args != null)
+            { SR2Console.SendError($"The '<color=white>{ID}</color>' command takes no arguments"); return false; }
+            return SilentExecute(args);
+        }
+        public override bool SilentExecute(string[] args)
+        {
+            SR2Console.Toggle();
+            return true;
+        }
+    }
+}
