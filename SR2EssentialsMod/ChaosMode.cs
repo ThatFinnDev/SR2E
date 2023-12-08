a96a496c57fa58566869fee3691a13cb577ccbfc
ThatFinn <finn@thatfinn.dev>
2023-12-08 23:38:04 +0100

    hi

--- SR2EssentialsMod/ChaosMode.cs
+++ SR2EssentialsMod/ChaosMode.cs
@@ -6,6 +6,14 @@ internal class ChaosMode
     {
         switch (sceneName)
         {
+            case "GameCore":
+                Color32 color = new Color32(242, 31, 124, 255);
+                SlimeDefinition tarr = SR2EUtils.Get<SlimeDefinition>("Tarr");
+                Material material = tarr.AppearancesDefault[0]._structures[0].DefaultMaterials[0];
+                material.SetColor("_TopColor", color);
+                material.SetColor("_MiddleColor", color);
+                material.SetColor("_BottomColor", color);
+                break;
             case "MainMenuEnvironment":
                 GameObject playerModel = GameObject.Find("BeatrixMainMenu");
                 playerModel.transform.localScale = new Vector3(-13.3182f, 2.0545f, -0.9782f);
