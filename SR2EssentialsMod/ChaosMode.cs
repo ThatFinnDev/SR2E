12cabfc8e48e2f81c6a99fc93f6134a58c27565b
ThatFinn <finn@thatfinn.dev>
2023-12-09 00:48:42 +0100

    replace plorts with slimes in market

--- SR2EssentialsMod/ChaosMode.cs
+++ SR2EssentialsMod/ChaosMode.cs
@@ -1,22 +1,92 @@
-﻿namespace SR2E;
+﻿using System.Linq;
+using Il2CppMonomiPark.SlimeRancher.UI;
+
+namespace SR2E;
 
 internal class ChaosMode
 {
     
+    [HarmonyPatch(typeof(MarketUI), "Start")]
+    public static class PatchMarketUIStart
+    {
+        public static void Prefix(MarketUI __instance)
+        {
+            if (SR2EEntryPoint.chaosMode)
+            {
+                __instance.plorts = newSlimesAsPlortEntries.ToArray();
+            }
+        }
+    }
 
+    [HarmonyPatch(typeof(EconomyDirector), "InitModel")]
+    public static class PatchEconomyDirectorInitModel
+    {
+        public static void Prefix(EconomyDirector __instance)
+        {
+            if (SR2EEntryPoint.chaosMode)
+            {
+                __instance.BaseValueMap = newValueMaps.ToArray();
+            }
+        }
+    }
+    public static List<MarketUI.PlortEntry> newSlimesAsPlortEntries = new List<MarketUI.PlortEntry>();
+    public static List<EconomyDirector.ValueMap> newValueMaps = new List<EconomyDirector.ValueMap>();
 
+    static void makeSlimesSellable(string nameWithInfo)
+    {
+        try
+        {
+            string[] split = nameWithInfo.Split("|");
+            string name = split[0];
+            float fullSaturation = float.Parse(split[1]);
+            float value = float.Parse(split[2]);
+            SlimeDefinition definition = SR2EUtils.Get<SlimeDefinition>(name);
+            if(definition==null)
+                return;
+            newSlimesAsPlortEntries.Add(new MarketUI.PlortEntry { identType = definition });
+            newValueMaps.Add(new EconomyDirector.ValueMap { Accept = definition.prefab.GetComponent<Identifiable>(), 
+                FullSaturation = fullSaturation, Value = value });
+        }
+        catch { }
+    }
+    static void switchSlimeAppearances(string slimeOne, string slimeTwo)
+    {
+        try
+        {
+            SlimeDefinition slimeOneDef = SR2EUtils.Get<SlimeDefinition>(slimeOne);
+            SlimeDefinition slimeTwoDef = SR2EUtils.Get<SlimeDefinition>(slimeTwo);
+            
+            var appearanceOne = slimeOneDef.AppearancesDefault; slimeOneDef.AppearancesDefault = slimeTwoDef.AppearancesDefault; slimeTwoDef.AppearancesDefault = appearanceOne;
+            
+            var icon = slimeOneDef.icon; slimeOneDef.icon = slimeTwoDef.icon; slimeTwoDef.icon = icon;
+            
+            var debugIcon = slimeOneDef.debugIcon; slimeOneDef.debugIcon = slimeTwoDef.debugIcon; slimeTwoDef.debugIcon = debugIcon;
+            
+        }
+        catch { }
+
+    }
     internal static void OnSceneWasLoaded(int buildIndex, string sceneName)
     {
         switch (sceneName)
         {
             case "GameCore":
-                Color32 color = new Color32(242, 31, 124, 255);
-                SlimeDefinition tarr = SR2EUtils.Get<SlimeDefinition>("Tarr");
-                Material material = tarr.AppearancesDefault[0]._structures[0].DefaultMaterials[0];
-                material.SetColor("_TopColor", color);
-                material.SetColor("_MiddleColor", color);
-                material.SetColor("_BottomColor", color);
-                tarr.prefab.GetComponent<AttackPlayer>().DamagePerAttack = 1000;
+                Color32 hotPink = new Color32(242, 31, 124, 255);
+                SlimeDefinition tarrDefinition = SR2EUtils.Get<SlimeDefinition>("Tarr");
+                Material tarrMaterial = tarrDefinition.AppearancesDefault[0]._structures[0].DefaultMaterials[0];
+                tarrMaterial.SetColor("_TopColor", hotPink);
+                tarrMaterial.SetColor("_MiddleColor", hotPink);
+                tarrMaterial.SetColor("_BottomColor", hotPink);
+                tarrDefinition.prefab.GetComponent<AttackPlayer>().DamagePerAttack = 1000;
+
+                switchSlimeAppearances("Gold", "Pink");
+                List<string> slimes = new List<string> { "Pink|40|7", "Cotton|30|15", "Phosphor|30|15", "Tabby|30|15", 
+                    "Angler|20|18","Rock|30|18", "Honey|20|18", "Boom|20|20",
+                    "Puddle|20|22", "Fire|20|22","Batty|20|18", "Crystal|20|20",
+                    "Hunter|20|20", "Flutter|20|20", "Ringtail|15|22", "Saber|15|24",
+                    "Yolky|15|28", "Tangle|15|27", "Dervish|15|27", "Gold|50000|200", "Lucky|15|27", "Tarr|15|27" };
+                for (int i = 0; i < slimes.Count; i++)
+                    makeSlimesSellable(slimes[i]);
                 break;
             case "MainMenuEnvironment":
                 GameObject playerModel = GameObject.Find("BeatrixMainMenu");
