9e7e43827414e5b07f2ad65024a4ef026d8b757b
ThatFinn <finn@thatfinn.dev>
2023-12-08 01:44:50 +0100

    Delete Fix

--- SR2EssentialsMod/Saving/SavePatches.cs
+++ SR2EssentialsMod/Saving/SavePatches.cs
@@ -104,6 +104,16 @@ public static class SavePatches
             SR2ESavableData.Instance.TrySave();
         }
     }
+    [HarmonyPatch(typeof(AutoSaveDirector), nameof(AutoSaveDirector.DeleteSave))]
+    public static class AutoSaveDirectorDeletePatch
+    {
+        public static void Prefix(AutoSaveDirector __instance, string saveName )
+        {
+            string path = Path.Combine(SystemContext.Instance.GetStorageProvider().Cast<FileStorageProvider>().savePath, $"{saveName}.sr2e");
+            if(File.Exists(path))
+                File.Delete(path);
+        }
+    }
 
     [HarmonyPatch(nameof(FileStorageProvider), "savePath", MethodType.Getter)]
     public static class StorageProviderPathGrabber
