6f61ae9869bd6d6a4ba333b0acb01c6eb6ef8c7b
ThatFinn <finn@thatfinn.dev>
2023-12-09 20:41:57 +0100

    Update GlobalUsings.cs

--- SR2EssentialsMod/GlobalUsings.cs
+++ SR2EssentialsMod/GlobalUsings.cs
@@ -3,6 +3,8 @@ global using Il2Cpp;
 global using MelonLoader;
 global using System.Collections.Generic;
 global using HarmonyLib;
+global using SR2E.Library;
+global using SR2E;
 global using Console = System.Console;
 global using Exception = System.Exception;
 global using Object = UnityEngine.Object;
