d55836a612d1c762a36ef773a1721ba888d32b90
MadMincraftr <thean.mcgarity@gmail.com>
2023-12-18 17:59:39 -0600

    stuff
    ignore the SR2EQuickButtons mod for now

---
+++ SR2EQuickButtons/SavableButtons.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SR2QuickButtons
+{
+    internal class SavableButtons
+    {
+        public List<ButtonData> mainMenuButtons = new List<ButtonData>();
+        public List<ButtonData> pauseButtons = new List<ButtonData>();
+        public List<ButtonData> ranchButtons = new List<ButtonData>();
+    }
+}
