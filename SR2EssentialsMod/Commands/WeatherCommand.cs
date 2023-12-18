9f58042c2a3f29e6bcea2c6dee7e15e697d4cf5c
AureumApes <aureumapes@duck.com>
2023-12-18 08:20:48 +0100

    Moved states and GetWeatherStateByName to an own file

--- SR2EssentialsMod/Commands/WeatherCommand.cs
+++ SR2EssentialsMod/Commands/WeatherCommand.cs
@@ -1,71 +1,47 @@
-﻿using Il2Cpp;
-using Il2CppMonomiPark.SlimeRancher.Persist;
+﻿using Il2CppMonomiPark.SlimeRancher.DataModel;
 using Il2CppMonomiPark.SlimeRancher.Weather;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace SR2E.Commands
+namespace SR2E.Commands;
+
+internal class WeatherCommand : SR2CCommand
 {
-    internal class WeatherCommand : SR2CCommand
+    public override string ID => "forceweather";
+
+    public override string Usage => "forceweather <state>";
+
+    public override string Description => "Force a weather state.";
+
+    public override string ExtendedDescription =>
+        "Allows you to make any weather in the game play where you are currently.";
+
+    public override bool Execute(string[] args)
     {
-        public static WeatherStateDefinition[] states => Resources.FindObjectsOfTypeAll<WeatherStateDefinition>();
+        if (args.Length != 1) return false;
 
-        internal static WeatherStateDefinition getWeatherStateByName(string name)
+        var def = Weather.getWeatherStateByName(args[0]);
+
+        if (def == null) return false;
+
+        var dir = Get<WeatherDirector>("WeatherVFX");
+
+        if (dir == null) return false;
+
+        var param = new WeatherModel.ZoneWeatherParameters
         {
-            foreach (WeatherStateDefinition state in states)
-                try
-                {
-                    if (state.name.ToUpper().Replace(" ", "") == name.ToUpper())
-                        return state;
-                }
-                catch (System.Exception ignored)
-                { }
+            WindDirection = new Vector3(45f, 0, 30f)
+        };
 
-            return null;
-        }
+        dir.RunState(def.Cast<IWeatherState>(), param);
+        SR2Console.SendMessage($"Successfully run weather state \"{def.name.Replace(" ", "")}\"");
+        return true;
+    }
 
-        public override string ID => "forceweather";
+    public override List<string> GetAutoComplete(int argIndex, string[] args)
+    {
+        var list = new List<string>();
 
-        public override string Usage => "forceweather <state>";
+        foreach (var state in Weather.states) list.Add(state.name.Replace(" ", ""));
 
-        public override string Description => "Force a weather state.";
-
-        public override string ExtendedDescription => "Allows you to make any weather in the game play where you are currently.";
-
-        public override bool Execute(string[] args)
-        {
-            if (args.Length != 1) return false;
-            
-            WeatherStateDefinition def = getWeatherStateByName(args[0]);
-            
-            if (def == null) return false;
-
-            var dir = Get<WeatherDirector>("WeatherVFX");
-
-            if (dir == null) return false;
-
-            var param = new Il2CppMonomiPark.SlimeRancher.DataModel.WeatherModel.ZoneWeatherParameters()
-            {
-                WindDirection = new Vector3(45f, 0, 30f)
-            };
-
-            dir.RunState(def.Cast<IWeatherState>(), param);
-            SR2Console.SendMessage($"Successfully run weather state \"{def.name.Replace(" ", "")}\"");
-            return true;
-        }
-
-        public override List<string> GetAutoComplete(int argIndex, string[] args)
-        {
-            var list = new List<string>();
-
-            foreach (var state in states)
-            {
-                list.Add(state.name.Replace(" ", ""));
-            }
-            return list;
-        }
+        return list;
     }
 }
