79273606716e561f0e51dfa81079e5d5f7d6700a
ThatFinn <finn@thatfinn.dev>
2024-06-06 22:26:41 +0200

    Fix

--- SR2EssentialsMod/Commands/RefillInvCommand.cs
+++ SR2EssentialsMod/Commands/RefillInvCommand.cs
@@ -20,21 +20,22 @@ public class RefillInvCommand : SR2Command
         int numberOfSlots = SceneContext.Instance.PlayerState.Ammo.Slots.Length - 1;
         int slotToFill = -1;
 
-        try
-        {
-            slotToFill = int.Parse(args[0]);
-        }
-        catch
-        {
-            return SendError(translation("cmd.error.notvalidint", args[0]));
-        }
+        if(args!=null)
+            try
+            {
+                slotToFill = int.Parse(args[0]);
+                if (slotToFill <= 0) return SendError(translation("cmd.error.notintabove", args[0]));
+                if (slotToFill > numberOfSlots)
+                    return SendError(translation("cmd.refillinv.error.slotdoesntexist", numberOfSlots));
+                slotToFill -= 1;
+            }
+            catch
+            {
+                return SendError(translation("cmd.error.notvalidint", args[0]));
+            }
 
-        if (slotToFill <= 0) return SendError(translation("cmd.error.notintabove", args[0]));
-        if (slotToFill > numberOfSlots)
-            return SendError(translation("cmd.refillinv.error.slotdoesntexist", numberOfSlots));
 
-        slotToFill -= 1;
-        if (slotToFill == -1)
+        if (args==null)
         {
             for (int i = 0; i < SceneContext.Instance.PlayerState.Ammo.Slots.Count; i++)
             {
@@ -56,7 +57,8 @@ public class RefillInvCommand : SR2Command
         if (SceneContext.Instance.PlayerState.Ammo.Slots[slotToFill].Id == null)
             return SendError(translation("cmd.refillinv.error.slotempty", slotToFill + 1));
 
-        SceneContext.Instance.PlayerState.Ammo.Slots[slotToFill].Clear();
+        Ammo.Slot invSlot = SceneContext.Instance.PlayerState.Ammo.Slots[slotToFill];
+        invSlot.Count = SceneContext.Instance.PlayerState.Ammo._ammoModel.GetSlotMaxCount(invSlot.Id, slotToFill);
         SendMessage(translation("cmd.refillinv.successsingle", slotToFill + 1));
 
 
