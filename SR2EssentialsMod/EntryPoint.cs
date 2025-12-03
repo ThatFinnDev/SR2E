176888fed5bc1cf756c3cb63dea20a15d2dba98d
ThatFinn <finn@thatfinn.dev>
2025-12-03 21:02:37 +0100

    3.4.1

--- SR2EssentialsMod/EntryPoint.cs
+++ SR2EssentialsMod/EntryPoint.cs
@@ -61,7 +61,7 @@ public static class BuildInfo
     /// For dev versions, use "-dev". Do not add a build number!<br />
     /// Add "+metadata" only in dev builds!
     /// </summary>
-    public const string DisplayVersion = "3.4.1-dev";
+    public const string DisplayVersion = "3.4.1";
 
     //allowmetadata, checkupdatelink,
     internal static readonly TripleDictionary<string, bool, string> PRE_INFO =
