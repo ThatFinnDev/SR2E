824132509cb2f4e50b46d31e75b069840711769e
ThatFinn <finn@thatfinn.dev>
2026-01-13 16:52:01 +0100

    3.6.0

--- SR2EssentialsMod/Managers/SR2EUpdateManager.cs
+++ SR2EssentialsMod/Managers/SR2EUpdateManager.cs
@@ -1,5 +1,9 @@
+using System;
 using System.Collections;
 using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using UnityEngine.Networking;
 
@@ -19,7 +23,15 @@ internal static class SR2EUpdateManager
         yield return uwr.SendWebRequest();
         if (uwr.isNetworkError || uwr.isHttpError) yield break;
         string json = uwr.downloadHandler.text;
-        try { JObject.Parse(json); }
+        try
+        {
+            var jobject = JObject.Parse(json);
+            if (!CheckSignature(jobject))
+            {
+                MelonLogger.Msg("SR2E API's signature is invalid, is the date & time set correctly?");
+                throw new Exception();
+            }
+        }
         catch { MelonLogger.Msg("SR2E API either changed or is broken."); yield break; }
         branchJson = json;
         MelonCoroutines.Start(CheckForNewVersion());
@@ -67,5 +79,39 @@ internal static class SR2EUpdateManager
                 MelonLogger.Msg("Restart needed for applying SR2E update");
             }
     }
+    
+    internal static bool CheckSignature(JObject jobject)
+    {
+        try
+        {
+            if (!jobject.ContainsKey("signature") || !jobject.ContainsKey("expires")) return false;
+            var signatureBase64 = jobject["signature"].Value<string>();
+            var expires = jobject["expires"].Value<long>();
 
+            if (DateTimeOffset.UtcNow.ToUnixTimeMilliseconds() > expires) return false;
+
+            var copy = (JObject)jobject.DeepClone();
+            copy.Remove("signature");
+            var dict = copy.ToObject<Dictionary<string, object>>();
+            var sortedDict = new SortedDictionary<string, object>(dict);
+            var canonicalJson = JsonConvert.SerializeObject(sortedDict, Formatting.None);
+    
+            var dataBytes = Encoding.UTF8.GetBytes(canonicalJson);
+            var signatureBytes = Convert.FromBase64String(signatureBase64);
+        
+            var pubKeyBase64 = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvW1COeevhDiQl5G4BbLTIZi+oGZNQSyorDs/ue1YGACa/KQEysp3akgFgR86sudcAnaA+b6qaR50hD+/Wd+ZofYxv8LRI1J6RqKWNzZDK8yJePo6eVbqPz1F/JP96+pNXHEGy7yJH0VRjO+h/Jb15vNfOSwfnoIWY4XUm/+LONTCTDXOfAS9HBHy6r3zc0AI0A+101Q8LYDhMrINiirVkQRZw4W5FzNF0Ouvj0emkQfyJ7pRZcD+GwrXhN5Sad65QeWF8joKq8aCcyEN0oifieDCtGyBCGbV+Jm3zElraS1NIKWDjQnT+b4QOJtSKRHSymVqX3oFWahV3cadi/CfqQIDAQAB"; 
+            var publicKeyBytes = Convert.FromBase64String(pubKeyBase64);
+
+            using (var rsa = RSA.Create())
+            {
+                rsa.ImportSubjectPublicKeyInfo(publicKeyBytes, out _);
+                return rsa.VerifyData(dataBytes, signatureBytes, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+            }
+        }
+        catch (Exception e)
+        {
+            MelonLogger.Error("Signature verification error: " + e.Message);
+            return false;
+        }
+    }
 }
