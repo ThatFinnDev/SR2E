16d93e7be914753ff08d67c5d66140d7f3434eb9
ThatFinn <finn@thatfinn.dev>
2026-01-13 19:23:20 +0100

    3.6.1

--- SR2EssentialsMod/EntryPoint.cs
+++ SR2EssentialsMod/EntryPoint.cs
@@ -41,7 +41,7 @@ public static class BuildInfo
     /// For dev versions, use "-dev". Do not add a build number!<br />
     /// Add "+metadata" only in dev builds!
     /// </summary>
-    public const string DisplayVersion = "3.6.1-dev";
+    public const string DisplayVersion = "3.6.1";
 
     //allowmetadata, checkupdatelink,
     internal static readonly TripleDictionary<string, bool, string> PRE_INFO = new ()
