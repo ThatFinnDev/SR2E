be392020890609caefd88a217d65ec5d682a62f8
ThatFinn <finn@thatfinn.dev>
2026-01-16 16:00:13 +0100

    Return to dev

--- SR2EssentialsMod/EntryPoint.cs
+++ SR2EssentialsMod/EntryPoint.cs
@@ -33,7 +33,7 @@ public static class BuildInfo
     public const string Description = "Essential stuff for Slime Rancher 2";
     public const string Author = "ThatFinn";
     public const string Contributors = "PinkTarr, shizophrenicgopher, Atmudia";
-    public const string CodeVersion = "3.6.2";
+    public const string CodeVersion = "3.6.3";
     public const string DownloadLink = "https://sr2e.sr2.dev/";
     public const string SourceCode = "https://github.com/ThatFinnDev/SR2E";
     public const string Nexus = "https://www.nexusmods.com/slimerancher2/mods/60";
@@ -46,7 +46,7 @@ public static class BuildInfo
     /// For dev versions, use "-dev". Do not add a build number!<br />
     /// Add "+metadata" only in dev builds!
     /// </summary>
-    public const string DisplayVersion = "3.6.2";
+    public const string DisplayVersion = "3.6.3-dev";
 
     //allowmetadata, checkupdatelink,
     internal static readonly TripleDictionary<string, bool, string> PRE_INFO = new ()
