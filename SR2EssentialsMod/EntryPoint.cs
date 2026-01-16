9458f9105c50f4e9cbf10189cb8383e5d28c528f
ThatFinn <finn@thatfinn.dev>
2026-01-16 15:57:17 +0100

    3.6.2
    I forgot to change the version

--- SR2EssentialsMod/EntryPoint.cs
+++ SR2EssentialsMod/EntryPoint.cs
@@ -46,7 +46,7 @@ public static class BuildInfo
     /// For dev versions, use "-dev". Do not add a build number!<br />
     /// Add "+metadata" only in dev builds!
     /// </summary>
-    public const string DisplayVersion = "3.6.2-dev";
+    public const string DisplayVersion = "3.6.2";
 
     //allowmetadata, checkupdatelink,
     internal static readonly TripleDictionary<string, bool, string> PRE_INFO = new ()
